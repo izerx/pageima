4f11c443912adaf3f9d5d5c6b2ebec094453dc04
Roman <romshtmb@gmail.com>
2020-03-30 21:37:10 +0300

    v 1.0.0

---
+++ App/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from selenium import webdriver
+from django.http import HttpResponse, JsonResponse
+from PIL import Image
+from io import BytesIO
+
+def index(request): 
+    return render (request, 'App/index.html')
+
+def API(request):
+    driver = webdriver.PhantomJS()
+    driver.set_window_size(1920, 1080)
+
+    try:
+        driver.get(request.GET.get('url'))
+    except Exception as e:
+        return HttpResponse(e.args)
+
+    img = driver.get_screenshot_as_png()
+
+    return HttpResponse(img, content_type="image/png")
